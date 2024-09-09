b1b5873d1116e348461aed525b850340c75e43cf
Georgina <88046437+GeorginaBerrezel@users.noreply.github.com>
2024-09-09 18:21:05 +0200

    Routing - root from items id

--- my-app/src/pages/_app.js
+++
@@ -1,9 +0,0 @@
-// src/pages/_app.js
-
-import '../styles/global.scss';  // Assure-toi que le chemin est correct
-
-function MyApp({ Component, pageProps }) {
-    return <Component {...pageProps} />;
-}
-
-export default MyApp;
