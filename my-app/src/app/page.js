f9555f6014d15c37fb534d2a6ac4340a402b6c4d
Georgina <88046437+GeorginaBerrezel@users.noreply.github.com>
2024-09-10 12:09:37 +0200

    Routing - Defining Routes and Pages

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -1,6 +1,14 @@
+/*
 import Image from "next/image";
+*/
+/*
 import styles from "./page.module.css";
+*/
+export default function Page() {
+  return <h1>Hello, Next.js!</h1>
+}
 
+/*
 export default function Home() {
   return (
       <main className={styles.main}>
@@ -98,4 +106,4 @@ export default function Home() {
         </div>
       </main>
   );
-}
+}*/
