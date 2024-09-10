d4896b66e1a7b166a4f57e41fbabe529abd405b0
Georgina <88046437+GeorginaBerrezel@users.noreply.github.com>
2024-09-10 14:53:44 +0200

    Routing - Linking : use component Link

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -4,8 +4,15 @@ import Image from "next/image";
 /*
 import styles from "./page.module.css";
 */
+import Link from 'next/link'
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>
+  return (
+      <div>
+        <h1>Hello, Next.js!</h1>
+        <Link href="/dashboard">Dashboard</Link>
+      </div>
+  )
 }
 
 
